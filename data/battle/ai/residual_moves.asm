dce3c4d857e471f8829d3664d03c6761b37be84b
Awakeon <Awakeon98@gmail.com>
2023-10-31 11:55:29 -0500

    Getting the base together

---
+++ data/battle/ai/residual_moves.asm
@@ -0,0 +1,14 @@
+; AI_CAUTIOUS discourages these moves after the first turn.
+
+ResidualMoves:
+	db LEECH_SEED
+	db POISONPOWDER
+	db STUN_SPORE
+	db THUNDER_WAVE
+	db FOCUS_ENERGY
+	db POISON_GAS
+	db TRANSFORM
+	db CONVERSION
+	db SUBSTITUTE
+	db SPIKES
+	db -1 ; end
